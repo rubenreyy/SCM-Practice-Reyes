9df4e47e0a87f425e5a8c9d08fad6310e41d5123
rubenreyy <ruben8reyed@gmail.com>
2025-03-25 23:58:38 -0500

    Return to original

--- test.py
+++ test.py
@@ -3,14 +3,5 @@ x = int(input("Enter a number: "))
 
 y = random.randint(1, 100)
 
-cont = True
-
-while (cont):
-    x = int(input("Enter a number: "))
-    if x > y:
-        print("Too high")
-    elif x < y:
-        print("Too low")
-    else:
-        print("Correct!")
-        cont = False
+if x == y:
+    print("You win!")
